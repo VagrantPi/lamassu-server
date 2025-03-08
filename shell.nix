c47c6a3ec2bddda9113aa48e23620906f3806363
Rafael Taranto <rafael.taranto@protonmail.com>
2025-03-08 12:28:23 +0000

    chore: migrating to nodejs 22

--- shell.nix
+++ shell.nix
@@ -1,13 +1,13 @@
 with import (fetchTarball {
-  name = "8ad5e8";
-  url = https://github.com/NixOS/nixpkgs/archive/8ad5e8132c5dcf977e308e7bf5517cc6cc0bf7d8.tar.gz;
-  sha256 = "17v6wigks04x1d63a2wcd7cc4z9ca6qr0f4xvw1pdw83f8a3c0nj";
+  name = "nixpkgs-194846768975b7ad2c4988bdb82572c00222c0d7";
+  url = https://github.com/NixOS/nixpkgs/archive/194846768975b7ad2c4988bdb82572c00222c0d7.tar.gz;
+  sha256 = "0snj72i9dm99jlnnmk8id8ffjnfg1k81lr7aw8d01kz3hdiraqil";
 }) {};
 
 stdenv.mkDerivation {
     name = "node";
-        buildInputs = [
-        nodejs-14_x
+    buildInputs = [
+        nodejs_22
         python3
         openssl
         postgresql
