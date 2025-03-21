b1e574ea7b5fedf17bf1d46e4032ca0b34fc31a4
Rafael Taranto <rafael.taranto@protonmail.com>
2025-03-21 18:25:52 +0000

    fix: loading svg on usdc

--- new-lamassu-admin/src/pages/Wallet/WizardSplash.jsx
+++ new-lamassu-admin/src/pages/Wallet/WizardSplash.jsx
@@ -9,7 +9,7 @@ import LitecoinLogo from 'src/styling/logos/icon-litecoin-colour.svg?react'
 import MoneroLogo from 'src/styling/logos/icon-monero-colour.svg?react'
 import TetherLogo from 'src/styling/logos/icon-tether-colour.svg?react'
 import TronLogo from 'src/styling/logos/icon-tron-colour.svg?react'
-import USDCLogo from 'src/styling/logos/icon-usdc-colour.svg'
+import USDCLogo from 'src/styling/logos/icon-usdc-colour.svg?react'
 import ZCashLogo from 'src/styling/logos/icon-zcash-colour.svg?react'
 
 import { Button } from 'src/components/buttons'
